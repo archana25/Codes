fa1d50bff77c657a5fdc3425c0bf6d9cab7e434b
Archana Mohan <mohanarchana2501@gmail.com>
2020-10-25 13:43:37 -0400

    Added iterators

--- C++/stl/vector_stl.cpp
+++ C++/stl/vector_stl.cpp
@@ -4,6 +4,13 @@
 
 using namespace std;
 
+bool comparator_function(int var_1,int var_2)
+{
+	// Returns comparator value
+	
+	return var_1>var_2;
+
+}
 int main(void)
 {
 	bool present;
@@ -16,7 +23,7 @@ int main(void)
 
 	//Searching(Binary Search (Ologn))
 	present = binary_search(test_vector.begin(),test_vector.end(),2321);
-	cout<<"Element 21 present:"<<present;
+	cout<<"Element 2321 present:"<<present;
 
 	//Push element at the end
 	test_vector.push_back(455);
@@ -29,9 +36,59 @@ int main(void)
 	//Find first occurrence of 455 in the vector
 	vector<int>::iterator lower_it = lower_bound(test_vector.begin(),test_vector.end(),455);
 	vector<int>::iterator upper_it = upper_bound(test_vector.begin(),test_vector.end(),455);
+
 	cout<<*lower_it<<*upper_it;
+
+	//To save time you can also use auto and C++ will understand that is is of iterator type
+	auto it = lower_bound(test_vector.begin(),test_vector.end(),1233);
+	cout<<"Lower bound of 1233"<<*it<<endl;
 	//Difference beween the iterators
 	//O(1)(Count no. of times 100 is occurring)
 	cout<<"Difference between the iterators"<<upper_it-lower_it;
+
+	//Printing the Vector
+	vector<int>::iterator index;
+	
+	for(index = test_vector.begin();index!=test_vector.end();index++)
+		cout<<"Element"<<*index<<endl;
+	//Sorting in descending order
+	
+	//sort(test_vector.begin(),test_vector.end(),greater<int>());
+	//for(index = test_vector.begin();index!=test_vector.end();index++)
+	//	cout<<"Element"<<*index<<endl;
+
+	sort(test_vector.begin(),test_vector.end(),comparator_function);
+	for(index = test_vector.begin();index!=test_vector.end();index++)
+		cout<<"Element"<<*index<<endl;
+	//Printing without iterator
+	cout<<"Without iterator";	
+	for(int parse:test_vector)
+	{
+		if(parse%2 == 1)
+			parse = 1;
+		cout<<parse<<" ";
+	}
+	cout<<"\n"<<"Parsing the vector after no reference";
+	for(int parse:test_vector)
+	{
+		cout<<parse<<" ";
+	}
+	//With reference you can update as well as parse
+	cout<<"With reference";
+	for(int &x:test_vector)
+	{
+		if(x%2 == 0)
+			x=2;
+		cout<<x<<" ";
+	}
+	cout<<"\n"<<"Parsing the vector after reference";
+	for(int parse:test_vector)
+	{
+		cout<<parse<<" ";
+	}
+
+
+
+
 
 }
